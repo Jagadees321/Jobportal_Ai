76b0e2888b837de6fe641f5cb6c05425974d3912
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-07-28 15:39:18 +0530

    login api done

--- router/userroutes.js
+++ router/userroutes.js
@@ -2,8 +2,9 @@
 import express from 'express'
 
 const router=express.Router();
-import {register} from '../controller/usercontroller.js'
-router.post('/register',register);
+import {login, register} from '../controller/usercontroller.js';
 
+router.post('/register',register);
+router.post('/login',login);
 
 export default router
