1d839c66579845bc0f52c7be84df7cf2ef17133d
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-07-31 16:30:00 +0530

    jobappliction done

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectdb from "./db/dbconnect.js";
 import userrouter from './router/userroutes.js';
 import jobrouter from './router/jobsroutes.js';
+import jobapplictionrouter from './router/jobapplicationroutes.js'
 import cors from 'cors'
 const app = express();
 dotenv.config();//load env variables
@@ -20,6 +21,7 @@ app.get('/api/home',(req,res)=>{
 //ROUTES
 app.use('/api',userrouter)
 app.use('/api',jobrouter)
+app.use('/api',jobapplictionrouter)
 let port=process.env.port || 5051;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
