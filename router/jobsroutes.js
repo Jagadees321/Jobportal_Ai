dfa5a3a838f86a3636a211c361d7a2d80a1464ef
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-07-31 12:44:19 +0530

    job api done

--- router/jobsroutes.js
+++ router/jobsroutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getalljobs, getjobbyid, postjob,updatejob} from '../controller/jobscontroller.js'
+import {deletejob, getalljobs, getjobbyid, postjob,updatejob} from '../controller/jobscontroller.js'
 const router=express.Router();
 
 //routes
@@ -7,4 +7,5 @@ router.post('/jobs/:adminid',postjob);
 router.get('/jobs',getalljobs);
 router.get('/jobs/:id',getjobbyid)
 router.put('/jobs/:id/:adminid',updatejob);
+router.delete('/jobs/:id/:adminid',deletejob);
 export default router
