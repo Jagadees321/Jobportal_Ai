fc5d2181d70c9f10b943c5ecff541d615c5d7f00
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-08-01 12:42:29 +0530

    appliction api done

--- router/jobapplicationroutes.js
+++ router/jobapplicationroutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
 const router=express.Router();
-import {applyjob, getapplicationsoflogineduser} from '../controller/jobapplicationcontroller.js';
+import {applyjob, getapplicationofparticularjob, getapplicationsoflogineduser} from '../controller/jobapplicationcontroller.js';
 
 
 router.post('/apply/:jobid/:userid',applyjob);
-router.get('/apply/:userid',getapplicationsoflogineduser);
-
+router.get('/applications/:userid',getapplicationsoflogineduser);
+router.get('/applicationsbyjobid/:jobid',getapplicationofparticularjob);
 
 export default router
