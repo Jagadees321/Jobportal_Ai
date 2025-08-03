e365ea6d461a5bf9b69c68460940205255001f90
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-08-03 12:59:14 +0530

    managing req size to 10 mb

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 dotenv.config();//load env variables
 connectdb(); 
 //MIDDLEWARES
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 app.get('/',(req,res)=>{
     res.status(200).send("server is running fine");
